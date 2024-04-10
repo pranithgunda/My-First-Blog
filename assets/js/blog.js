4aede8fa8387e5961f77bb7c78e6e12e11aa14a2
Pranith Gunda <pranith0527@yahoo.com>
2024-04-10 13:20:24 -0400

    Dark and Light mode functionality changes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,6 @@
 const backButton = document.getElementById('back');
+const container = document.querySelector('.container');
+const toggleButton = document.getElementById('darklightmode-toggle');
 
 // navigate back to form on click of back button
 backButton.addEventListener('click',function (event) {
@@ -6,5 +8,22 @@ backButton.addEventListener('click',function (event) {
     location.href = "index.html";
 });
 
-const toggleButton = document.getElementById('darklightmode-toggle');
-console.log(toggleButton);
+// set default mode to light
+
+let mode = 'light';
+
+// listen for click event on toggle button
+toggleButton.addEventListener('click', function() {
+
+    // If mode is light apply dark background and styles by setting class attribute
+    if(mode==='light'){
+        mode = 'dark';
+        container.setAttribute('class','dark');
+    }
+    // Else apply light background and styles by removing class attribute
+    else{
+        mode = 'light';
+        container.removeAttribute('class','dark');
+    }
+
+});
